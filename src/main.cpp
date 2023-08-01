e948025c072a4f15fb88d00b74c6175c76d6d439
Chesko <cheskoart9m@gmail.com>
2023-08-02 00:24:24 +0300

    Implemented main tasks

--- src/main.cpp
+++ src/main.cpp
@@ -3,53 +3,94 @@
 #include "Helix.h"
 
 #include <iostream>
+#include <iomanip>
 #include <memory>
 #include <numbers>
+#include <numeric>
+#include <random>
+#include <ranges>
 
 int main()
 {
-  const Real PI = std::numbers::pi_v<Real>;
+  // Random filling.
+  std::random_device aRandDev;
+  std::mt19937 anEngine{ aRandDev() };
+  std::uniform_int_distribution<int> aCurveTypeGenerator{ 0, 2 };
+  std::uniform_real_distribution<Real> aRadiiGenerator{ -0.3f, 4.0f };
 
+  const int nbDesiredCurves = 30;
+  using CurvePtr = std::shared_ptr<Curve3D>;
+  std::vector<CurvePtr> aFirstContainer;
+  for (int aCurrentCurve = 0; aCurrentCurve < nbDesiredCurves; aCurrentCurve++)
   {
-    auto aCirclePtr = std::make_shared<Circle>(1.0f);
-    Real aRadius = aCirclePtr->GetRadius();
-    std::shared_ptr<Curve3D> aCircleBasePtr = aCirclePtr;
-
-    Point3D aPnt = aCircleBasePtr->GetPointAt(PI / 5.0f);
-    Vector3D aDer = aCircleBasePtr->GetFirstDerivativeAt(PI / 5.0);
-
-    std::cout << "Radius = " << aRadius << "\n";
-    std::cout << "Point = " << aPnt << "\n";
-    std::cout << "Derivative = " << aDer << "\n\n";
+    try
+    {
+      int aType = aCurveTypeGenerator(anEngine);
+      switch (aType)
+      {
+      case 0:
+        aFirstContainer.push_back(std::make_shared<Circle>(aRadiiGenerator(anEngine)));
+        break;
+      case 1:
+        aFirstContainer.push_back(std::make_shared<Ellips>(aRadiiGenerator(anEngine), aRadiiGenerator(anEngine)));
+        break;
+      case 2:
+        aFirstContainer.push_back(std::make_shared<Helix>(aRadiiGenerator(anEngine), aRadiiGenerator(anEngine)));
+        break;
+      default:
+        break;
+      }
+    }
+    // Handling physical uncorrectness
+    catch (std::invalid_argument anExc)
+    {
+      std::cout << anExc.what() << "\n";
+      aCurrentCurve--;
+    }
   }
 
+  // Printing at Pi/4
+  const Real aParamOfInterest = std::numbers::pi_v<Real> / 4.0f;
+  std::cout << std::fixed << std::setprecision(3);
+  for (int anIdx = 0; const auto & aCurve : aFirstContainer)
   {
-    auto anEllipsPtr = std::make_shared<Ellips>(2.5f, 3.2f);
-    Real aRadiusX = anEllipsPtr->GetRadiusX();
-    Real aRadiusY = anEllipsPtr->GetRadiusY();
-    std::shared_ptr<Curve3D> anEllipsBasePtr = anEllipsPtr;
-    Point3D aPnt = anEllipsBasePtr->GetPointAt(PI / 5.0);
-    Vector3D aDer = anEllipsBasePtr->GetFirstDerivativeAt(PI / 5.0);
+    std::cout << "Curve #" << anIdx << "'s \tPoint = " << aCurve->GetPointAt(aParamOfInterest)
+              << "\tFirst derivative = " << aCurve->GetFirstDerivativeAt(aParamOfInterest) << "\n";
 
-    std::cout << "RadiusX = " << aRadiusX << "\n";
-    std::cout << "RadiusY = " << aRadiusY << "\n";
-    std::cout << "Point = " << aPnt << "\n";
-    std::cout << "Derivative = " << aDer << "\n\n";
+    anIdx++;
   }
 
+  // Filtering
+  auto isCircle = [](const auto & theCurve)
   {
-    auto aHelixPtr = std::make_shared<Helix>(1.2f, 2.4f);
-    Real aRadius = aHelixPtr->GetRadius();
-    Real aStep = aHelixPtr->GetStep();
-    std::shared_ptr<Curve3D> aHellixBasePtr = aHelixPtr;
-    Point3D aPnt = aHellixBasePtr->GetPointAt(PI / 8.0);
-    Vector3D aDer = aHellixBasePtr->GetFirstDerivativeAt(PI / 8.0);
+    return std::dynamic_pointer_cast<Circle>(theCurve) != nullptr;
+  };
+  auto anOnlyCirclesView = aFirstContainer | std::views::filter(isCircle);
+  std::vector<CurvePtr> aSecondContainer(anOnlyCirclesView.begin(), anOnlyCirclesView.end());
 
-    std::cout << "Radius = " << aRadius << "\n";
-    std::cout << "Step = " << aStep << "\n";
-    std::cout << "Point = " << aPnt << "\n";
-    std::cout << "Derivative = " << aDer << "\n\n";
+  // Sorting
+  std::ranges::sort (aSecondContainer, std::less<>{}, [](const auto& theCircle)
+    {
+      return std::static_pointer_cast<Circle>(theCircle)->GetRadius();
+    });
+  for (int anIdx = 0; const auto & aCircle : aSecondContainer)
+  {
+    std::cout << "Circle #" << anIdx << "'s \tRadius = " << std::static_pointer_cast<Circle>(aCircle)->GetRadius() << "\n";
+    anIdx++;
   }
+
+  // Sum of radii sequential
+  Real aSum1 = std::reduce(std::next(aSecondContainer.begin()), aSecondContainer.end(),
+                           std::static_pointer_cast<Circle>(aSecondContainer[0])->GetRadius(),
+                           [](Real theAccumulatedValue, const auto& theCircle)
+    {
+      return theAccumulatedValue + std::static_pointer_cast<Circle>(theCircle)->GetRadius();
+    });
+  std::cout << "Sum of radii = " << aSum1 << "\n";
+
+  // TODO:
+  //   OpenMP reduce
+  //   shared lib
 
   return 0;
 }
